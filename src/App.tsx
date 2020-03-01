3e05fa540c90677dedd49836f09683f9b9a845ab
Amaus-p <61465352+Amaus-p@users.noreply.github.com>
2020-03-01 15:31:40 +0100

    Update Poke1

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,63 @@ import PropTypes from 'prop-types';
 import './App.css';
 
 class App extends Component {
-
+  
     state = {
-      pokemonData : {
-        pokeId : 0,
-        pokeName : '',
-        pokeType : '',
-        pokeCategory : '',
-        pokeHigh : '',
-        pokeWeight : '',
+          ID : 0,
+          pokeId : 0,
+          pokeName : '',
+          pokeType : '',
+          pokeCategory : '',
+          pokeHeight : '',
+          pokeWeight : '',
+          pokeAbility1 : '',
+          pokeAbility2 : '',
+          pokePhotoBack : '',
+          pokePhotoFront : '',
+      }    
+  
+    // function for binding but I don't understand why do we have to bind it
+    //I would like to put ID in argument in getData but it raises an error, I don't know why
+    //So I put ID in state but ID should be a name or a figure so there is a type problem
+    getData = () => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/pikachu/`)
+        .then((response) => {
+          return response.json();
+        })          
+        .then((data) => {this.setState({
+          pokeId : data.id,
+          pokeName : data.name,
+          pokeType : data.types[0].type.name,
+          pokeAbility1 : data.abilities[0].ability.name,
+          pokeAbility2 : data.abilities[1].ability.name,
+          pokeCategory : data.species.name,
+          pokeHeight : data.height,
+          pokeWeight : data.weight,
+          pokePhotoBack : data.sprites.back_default,
+          pokePhotoFront : data.sprites.front_default,
+        })})
       }
-    }
 
-    function GetData (ID){
-      fetch(`https://pokeapi.co/api/v2/pokemon/${ID}/`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {this.pokemonData.setState({
-        pokeId : data.results.id,
-        pokeName : data.results.name,
-        pokeType : '',
-        pokeCategory : '',
-        pokeHigh : '',
-        pokeWeight : '',
-      })})
+    render () {
+      
+      const {pokeId, pokeName, pokeType, pokeAbility1, pokeAbility2, pokeCategory, pokeHeight,pokeWeight, pokePhotoBack, pokePhotoFront} = this.state;
+
+      this.getData();
+
+      return(
+      <div> 
+          <h1> Votre Pokédex </h1>
+          <br/>
+            <img> {pokePhotoBack}</img>
+            <img> {pokePhotoFront}</img>
+            <h2> {pokeName} No {pokeId} </h2><br/>
+            <h3>Taille : 0.{pokeHeight}m</h3><br/>
+            <h3>Poids : {pokeWeight}kg</h3><br/>
+            <h3>Catégorie : {pokeCategory}</h3><br/>
+            <h3>Talent : {pokeAbility1} , {pokeAbility2}</h3><br/>
+            <h3>Type : {pokeType}</h3><br/>
+      </div>
+      )
     }
 
 }
