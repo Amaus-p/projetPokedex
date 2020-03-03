aba9b9240d3b080d49093cc5c9982301b195a3a0
Amaus-p <61465352+Amaus-p@users.noreply.github.com>
2020-03-03 09:04:39 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import PropTypes, { array } from 'prop-types';
 import './App.css';
 
 class App extends Component {
@@ -16,13 +16,16 @@ class App extends Component {
           pokeAbility2 : '',
           pokePhotoBack : '',
           pokePhotoFront : '',
+          familyMember1 : ['' , 0, '']  ,    // Name, ID, photo
+          familyMember2 : ['' , 0, ''] ,      //LA COMMANDE SCR LIGNE 150 A UN PROBLEME AVEC QQCH DANS CETTE STRUCTURE DE DONNEES?
+          familyMember3 : ['' , 0, ''] ,      
           Next : false,             //allows to stick to one pokemon in the pokedex
-          init: false,              //initialisation pb avec initialisation ligne 60 apparemment
+          init: false,              //clear the page if init = false
       }    
   
     // function for binding but I don't understand why do we have to bind it
     // in tsx arguments' type have to be indicated in the brackets
-    getData = (ID : number | string) => {
+    getPokeData = (ID : number | string) => {
         fetch(`https://pokeapi.co/api/v2/pokemon/${ID}/`)
         .then((response) => {
           return response.json();
@@ -41,6 +44,63 @@ class App extends Component {
           Next : false,
         })})
       }
+
+      getPokeSpeciesData = (ID : number | string) => {
+        fetch(`https://pokeapi.co/api/v2/pokemon-species/${ID}/`)
+          .then((response) => {
+            return response.json();
+          })          
+          .then((data) => {
+            let evolutionChainUrl = data.evolution_chain.url        //gives the url of the evolution chain in order to make the 
+            fetch(evolutionChainUrl)
+              .then((response) => {
+                return response.json();
+              })          
+              .then((data) => {
+            this.setState({
+              familyMember1 : [data.chain.species.name,this.state.familyMember1[1],this.state.familyMember1[2]],
+              familyMember2 : [data.chain.evolves_to[0].species.name,this.state.familyMember2[1],this.state.familyMember2[2]],
+              familyMember3 : [data.chain.evolves_to[0].evolves_to[0].species.name,this.state.familyMember2[1],this.state.familyMember2[2]],
+             })
+             this.getFamilyInfo()
+          })
+        })
+      }
+
+      getFamilyInfo = () => {
+        const {familyMember1,familyMember2,familyMember3} =this.state
+        fetch(`https://pokeapi.co/api/v2/pokemon/${familyMember1[0]}/`)
+        .then((response) => {
+          return response.json();
+        })          
+        .then((data) => {
+
+          this.setState({
+            familyMember1 : [familyMember1[0],data.id,data.sprites.front_default]
+        })
+        })
+        fetch(`https://pokeapi.co/api/v2/pokemon/${familyMember2[0]}/`)
+        .then((response) => {
+          return response.json();
+        })          
+        .then((data) => {
+
+          this.setState({
+            familyMember2 : [familyMember2[0],data.id,data.sprites.front_default]
+        })})
+        fetch(`https://pokeapi.co/api/v2/pokemon/${familyMember3[0]}/`)
+        .then((response) => {
+          return response.json();
+        })          
+        .then((data) => {
+
+          this.setState({
+            familyMember3 : [familyMember3[0],data.id,data.sprites.front_default]
+        })})
+      }
+    
+
+
     //fx for binding
     handleChange = (input : string | number) => (event : any) => {
         this.setState({[input]: event.target.value});
@@ -53,9 +113,12 @@ class App extends Component {
 
     render () {
 
-      const {pokeId, pokeName, pokeType, pokeAbility1, pokeAbility2, pokeCategory, pokeHeight,pokeWeight, pokePhotoBack, pokePhotoFront,Next,init} = this.state;
+      const {pokeId, pokeName, pokeType, pokeAbility1, pokeAbility2, pokeCategory, pokeHeight,pokeWeight, 
+              pokePhotoBack, pokePhotoFront,Next,init,familyMember1,familyMember2,familyMember3} = this.state;
       let {ID} = this.state;
-
+      let url1 = familyMember1[2];
+      console.log(familyMember1)
+      console.log(3)
       if (init===false) {
         return(
           <div style={{textAlign: "center",}}>
@@ -69,7 +132,7 @@ class App extends Component {
       }
 
       else if (init===true) {
-        if (Next===false){
+        if (Next===false){      // pour garder ce pokémon sur la page jusqu'au prochain clic sur "rechercher"
           return(
             <div style={{textAlign: "center",}}>
               <h1> Pokédex </h1> <br/>
@@ -82,7 +145,12 @@ class App extends Component {
               <h3>Poids : {pokeWeight}kg</h3>
               <h3>Catégorie : {pokeCategory}</h3>
               <h3>Talent : {pokeAbility1} , {pokeAbility2}</h3>
-              <h3>Type : {pokeType}</h3> 
+              <h3>Type : {pokeType}</h3> <br/>
+              <h2> Evolution </h2>
+              <img src = {familyMember1[2]}/> <img src = {familyMember2[2]}/> <img src = {familyMember3[2]}/>
+              <h3> {familyMember1[0]} No {familyMember1[1]}  
+              {familyMember2[0]}  No {familyMember2[1]}  
+              {familyMember3[0]} No{familyMember3[1]} </h3>
             </div>
             )
           }
@@ -90,7 +158,11 @@ class App extends Component {
   
         else if (Next ===true )
           {
-            this.getData (ID);
+            
+            this.getPokeData(ID);
+            this.getPokeSpeciesData(ID);
+            console.log(familyMember1)
+            console.log(4)
             return(
               <div style={{textAlign: "center",}}> 
                 <h1> Pokédex </h1> <br/>
@@ -103,7 +175,14 @@ class App extends Component {
                   <h3>Poids : {pokeWeight}kg</h3>
                   <h3>Catégorie : {pokeCategory}</h3>
                   <h3>Talent : {pokeAbility1} , {pokeAbility2}</h3>
-                  <h3>Type : {pokeType}</h3>       
+                  <h3>Type : {pokeType}</h3>     
+                  <h2> Evolution </h2><br/>
+                  <img src = {familyMember1[2]} alt={familyMember1[0]}/> <img src = {familyMember2[2]}/> <img src = {familyMember3[2]}/>
+                  <h3> {familyMember1[0]} No {familyMember1[1]}  
+                  {familyMember2[0]}  No {familyMember2[1]}  
+                  {familyMember3[0]} No{familyMember3[1]} </h3>
+
+
               </div>
           )
           }            
@@ -112,6 +191,6 @@ class App extends Component {
 
 }
 
-
-
+//<img src = {familyMember1[2]}/> <img src = {familyMember2[2]}/> <img src = {familyMember3[2]}/>
 export default App;
+
