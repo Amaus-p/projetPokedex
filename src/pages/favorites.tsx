d9ccae80d70fd037b60b5c97b1d4e31b719ecc6b
Amaus-p <61465352+Amaus-p@users.noreply.github.com>
2020-03-09 10:54:46 +0100

    Add files via upload

---
+++ src/pages/favorites.tsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import PropTypes, { array, string, number } from 'prop-types';
+import './App.css';
+
+class Favorites extends Component{
+
+    render(){
+
+        let listOfFavoritesPokemons = [];
+
+        
+        return(
+            'work in progress'
+        )
+    }
+}
+
+export default Favorites
